7194c2b1688cfef1b50eebfdc58a9ae72b3670ae
Vishnu11Venkat <vishnu4venkat@gmail.com>
2023-06-03 13:35:46 +0530

    ser

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,14 +6,27 @@ import { LoginService } from './login/login.service';
 import { LoginModule } from './login/login.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import mongoose from 'mongoose';
 require('dotenv').config();
 
 @Module({
   imports: [
     // 'mongodb://login:123@localhost:27017/login'
     MongooseModule.forRoot(process.env.MONGO_URI),
+    // const conn = await mongoose.connect(process.env.MONGO_URI)
      LoginModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+
+   connectDB = async () => {
+    try {
+      const conn = await mongoose.connect(process.env.MONGO_URI);
+      console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+      console.log(error);
+      process.exit(1);
+    }
+  }
+}
